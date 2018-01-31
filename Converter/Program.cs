8f7a0d0fbed2af14c8efb64970fb053398e45d5e
Zemiri Rangel <rangelzemiri@ahoo.com>
2018-01-30 20:09:33 -0600

    first commit

---
+++ Converter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Converter App");
+            Console.WriteLine("Description: Will convert yards to inches");
+
+            int numYards = int.Parse(Console.ReadLine());
+
+            int numInches = numYards * 36;
+
+            Console.WriteLine("Number of Inches in " + numYards + " yards is " + numInches + "inches");
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
